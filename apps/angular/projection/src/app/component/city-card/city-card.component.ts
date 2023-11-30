79fe1daa32ad46785e253670e74290ebeb33e096
Sean <stmclarke@gmail.com>
2023-11-30 15:58:30 +0000

    feat: refactor of card component

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `<app-card [list]="(cities$ | async) || []" (add)="addNewItem()">
+    <img image src="assets/img/city.jpg" width="200px" />
+    <ng-template #listItem let-city
+      ><app-list-item (delete)="deleteItem(city.id)">
+        {{ city.name }}
+      </app-list-item></ng-template
+    >
+  </app-card> `,
   standalone: true,
-  imports: [],
+  imports: [CardComponent, ListItemComponent, AsyncPipe],
+  styles: [
+    `
+      :host {
+        --card-background-color: rgba(0, 0, 250, 0.1);
+      }
+    `,
+  ],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  #store = inject(CityStore);
+  #http = inject(FakeHttpService);
+  cities$ = this.#store.cities$;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.#http.fetchCities$.subscribe((c) => this.#store.addAll(c));
+  }
+
+  deleteItem(id: number) {
+    this.#store.deleteOne(id);
+  }
+
+  addNewItem() {
+    this.#store.addOne(randomCity());
+  }
 }
--- apps/angular/projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,35 +1,50 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
-import { Teacher } from '../../model/teacher.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-teacher-card',
-  template: `<app-card
-    [list]="teachers"
-    [type]="cardType"
-    customClass="bg-light-red"></app-card>`,
+  template: `<app-card [list]="(teachers$ | async) || []" (add)="addOne()">
+    <img image src="assets/img/teacher.png" width="200px" />
+    <ng-template #listItem let-teacher
+      ><app-list-item (delete)="deleteOne(teacher.id)">
+        {{ teacher.firstname }}
+      </app-list-item></ng-template
+    >
+  </app-card>`,
   styles: [
     `
-      ::ng-deep .bg-light-red {
-        background-color: rgba(250, 0, 0, 0.1);
+      :host {
+        --card-background-color: rgba(250, 0, 0, 0.1);
       }
     `,
   ],
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, ListItemComponent, AsyncPipe],
 })
 export class TeacherCardComponent implements OnInit {
-  teachers: Teacher[] = [];
-  cardType = CardType.TEACHER;
+  teachers$ = this.store.teachers$;
 
-  constructor(private http: FakeHttpService, private store: TeacherStore) {}
+  constructor(
+    private http: FakeHttpService,
+    private store: TeacherStore,
+  ) {}
 
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
+  }
 
-    this.store.teachers$.subscribe((t) => (this.teachers = t));
+  addOne() {
+    this.store.addOne(randTeacher());
+  }
+
+  deleteOne(id: number) {
+    this.store.deleteOne(id);
   }
 }
