766fa8645bf05d3a9a66e8a43a12806175589514
Thomas Syczyk <tomasz.syczyk@turntown.digital>
2024-01-25 16:55:17 +0000

    feat: migrated app to standalone architecture

--- apps/angular/module-to-standalone/src/main.ts
+++ apps/angular/module-to-standalone/src/main.ts
@@ -1,6 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter([])],
+}).catch((err) => console.error(err));
