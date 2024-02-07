083d8cd90e996fa1532fd08b6f0a6fb104aec37f
Sean <stmclarke@gmail.com>
2024-02-07 16:00:55 +0000

    feat: completed challenge

--- apps/angular/injection-token/src/app/data.ts
+++ apps/angular/injection-token/src/app/data.ts
@@ -1,1 +1,10 @@
-export const DEFAULT_TIMER = 1000;
+import { InjectionToken } from '@angular/core';
+
+export const TIMER = new InjectionToken<number>('timer', {
+  factory: () => 1_000,
+});
+
+export const getTimerProvider = (value: number) => ({
+  provide: TIMER,
+  useValue: value,
+});
