66efa80c4b345ffd775254a0eb6be11018dd86e2
Matthew Fox <matthew.nutter@turntown.digital>
2024-03-20 16:44:15 +0000

    feat(challenge-39): improvements

--- apps/angular/injection-token/src/app/data.ts
+++ apps/angular/injection-token/src/app/data.ts
@@ -1,7 +1,11 @@
-import { InjectionToken } from '@angular/core';
+import { InjectionToken, Provider } from '@angular/core';
 
 export const DEFAULT_TIMER = 1000;
 
 export const TIMER = new InjectionToken<number>('timer', {
   factory: () => DEFAULT_TIMER,
 });
+
+export function provideTimer(value: number): Provider {
+  return { provide: TIMER, useValue: value };
+}
