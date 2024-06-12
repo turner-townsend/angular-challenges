a8ad69951928d04dfcc7dc38f94c7c58e38f5793
Sean <stmclarke@gmail.com>
2024-06-12 16:04:47 +0100

    feat: added worker to application

--- apps/performance/40-web-workers/src/app/app.component.ts
+++ apps/performance/40-web-workers/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeavyCalculationService } from './heavy-calculation.service';
 import { UnknownPersonComponent } from './unknown-person/unknown-person.component';
 
@@ -9,24 +9,31 @@ import { UnknownPersonComponent } from './unknown-person/unknown-person.componen
   providers: [HeavyCalculationService],
   selector: 'app-root',
   template: `
-    <unknown-person [step]="loadingPercentage()" class="relative grow" />
+    <unknown-person [step]="workerLoadingPercentage()" class="relative grow" />
     <button
       class="my-3 w-fit self-center rounded-md border border-white px-4 py-2 text-2xl text-white"
       (click)="discover()">
       Discover
     </button>
-    <div class="p-1 text-white">Progress: {{ loadingPercentage() }}%</div>
+    <div class="p-1 text-white">Progress: {{ workerLoadingPercentage() }}%</div>
   `,
   host: {
     class: `flex flex-col h-screen w-screen bg-[#1f75c0]`,
   },
 })
 export class AppComponent {
-  private heavyCalculationService = inject(HeavyCalculationService);
-
-  readonly loadingPercentage = this.heavyCalculationService.loadingPercentage;
+  workerLoadingPercentage = signal(0);
 
   discover() {
-    this.heavyCalculationService.startLoading();
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker(
+        new URL('./heavy-calculation.worker', import.meta.url),
+      );
+      worker.onmessage = ({ data }) => {
+        this.workerLoadingPercentage.set(data);
+      };
+      worker.postMessage('start');
+    }
   }
 }
