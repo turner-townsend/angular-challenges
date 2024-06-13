a68edc3ab0726280b7ee67b3f59959c4452650b1
Matthew Fox <matthew.nutter@turntown.digital>
2024-06-13 17:26:42 +0100

    feat(challenge-40): solution

--- apps/performance/40-web-workers/src/app/heavy-calculation.service.ts
+++ apps/performance/40-web-workers/src/app/heavy-calculation.service.ts
@@ -1,16 +1,30 @@
-import { Injectable, computed, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 
 @Injectable()
 export class HeavyCalculationService {
   private finalLength = 664579;
   private loadingLength = signal(0);
+  private worker?: Worker;
 
   loadingPercentage = computed(
     () => (this.loadingLength() * 100) / this.finalLength,
   );
 
   startLoading() {
-    this.randomHeavyCalculationFunction();
+    if (this.worker) {
+      this.worker.terminate();
+      this.loadingLength.set(0);
+    }
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      this.worker = new Worker(new URL('./heavy.worker', import.meta.url));
+      this.worker.onmessage = () => {
+        this.loadingLength.update((l) => l + 1);
+      };
+      this.worker.postMessage('');
+    } else {
+      this.randomHeavyCalculationFunction();
+    }
   }
 
   private randomHeavyCalculationFunction() {
